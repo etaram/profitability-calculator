a64af7cd2b08ae8dace9c885f31f9d8ed37179ba
etaram <32324612+etaram@users.noreply.github.com>
2024-09-07 15:33:08 +0300

    Create CardContent.jsx

---
+++ public/src/components/ui/CardContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardContent = ({ children, className }) => {
+  return (
+    <div className={`text-gray-700 ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default CardContent;
