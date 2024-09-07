93e9cd6c1debf9c8ddc3dfc20182df5324cf3734
etaram <32324612+etaram@users.noreply.github.com>
2024-09-07 15:35:17 +0300

    Create App.jsx

---
+++ public/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfitabilityCalculator from './components/ProfitabilityCalculator';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProfitabilityCalculator />
+    </div>
+  );
+};
+
+export default App;
